7d70544cdcfc9ac9a46b74e1d5283e5b85cca82c
Aleksei Prokopov <robby.roboter@gmail.com>
2019-12-18 17:06:22 +0200

    remove everything unrelated
    npx ngx-translate-extract --marker _ --input ./src --output ./src/assets/locale/en.json --clean --sort --format namespaced-json

--- healthcheck/src/app/home/home.page.ts
+++ healthcheck/src/app/home/home.page.ts
@@ -9,30 +9,14 @@ import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 })
 export class HomePage {
   result;
-  constructor(public translateService: TranslateService) { }
+  constructor(public feedbackService: TranslateService) { }
 
-  doahealthcheck(url) {
-    this.http.get(url, { headers: new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' }) }).subscribe(
-      result => {
-        this.result = 'success' + JSON.stringify(result);
-      },
-      error => {
-        this.result = 'error' + JSON.stringify(error);
-      }
-    );
-  }
 
   onSubmit() {
-    this.feedbackService.submit({}).subscribe(xx => {
-      this.translateService.get(_('FEEDBACK.thankyou-msg')).subscribe((message: string) => {
-        this.toastController.create({
-          message,
-          duration: 3000
-        }).then(x => x.present());
-      });
-      this.form.reset();
-      this.isRated = false;
-      this.router.navigate(['/']);
+
+    this.feedbackService.get(_('FEEDBACK.thankyou-msg')).subscribe((message: string) => {
+    //
     });
+
   }
 }
