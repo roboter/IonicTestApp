54058c55ee06e160f9a316efb783b404e88e7434
Aleksei Prokopov <robby.roboter@gmail.com>
2019-12-18 17:02:51 +0200

    error is here

--- healthcheck/src/app/home/home.page.ts
+++ healthcheck/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class HomePage {
   result;
-  constructor(private http: HttpClient) { }
+  constructor(public feedbackService: FeedbackService,public toastController: ToastController,public translateService: TranslateService,public device: Device,private router: Router) { }
 
   doahealthcheck(url) {
     this.http.get(url, { headers: new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' }) }).subscribe(
@@ -19,5 +20,27 @@ export class HomePage {
         this.result = 'error' + JSON.stringify(error);
       }
     );
+  }
+
+  onSubmit() {
+    this.feedbackService.submit({}).subscribe(xx => {
+      this.translateService.get(_('FEEDBACK.thankyou-msg')).subscribe((message: string) => {
+        this.toastController.create({
+          message,
+          duration: 3000
+        }).then(x => x.present());
+      });
+      this.form.reset();
+      this.isRated = false;
+      this.router.navigate(['/']);
+    }, _ => {
+      this.translateService.get('GENERAL.general-error-msg').subscribe((message: string) => {
+        this.toastController.create({
+          message,
+          showCloseButton: true,
+          duration: 3000
+        }).then(x => x.present());
+      });
+    });
   }
 }
