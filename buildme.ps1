cb5468459a17d7c053dbc94cdf6b606ee610c4e2
Aleksei Prokopov <aleksei.prokopov@novater.com>
2021-01-11 17:11:33 +0200

    codemagic

---
+++ buildme.ps1
@@ -0,0 +1,5 @@
+npm run build
+npx cap copy android
+npx cap sync android
+npx cap update android
+npx cap open android
--- healthcheck/buildme.ps1
+++
@@ -1,5 +0,0 @@
-npm run build
-npx cap copy android
-npx cap sync android
-npx cap update android
-npx cap open android
