fb962a416d57929e4de586dbc2c296b3efa4bc81
Aleksei Prokopov <roboter@begemotik.ee>
2023-06-18 13:14:18 +0300

    Inside

--- healthcheck/src/app/app-routing.module.ts
+++ healthcheck/src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  {
+    path: 'inside',
+    loadChildren: () => import('./pages/inside/inside.module').then( m => m.InsidePageModule)
+  },
+
 ];
 
 @NgModule({
