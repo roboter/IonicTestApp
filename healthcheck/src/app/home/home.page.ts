b6a2dbec32919dd49bee7477ba21de532460db20
Aleksei Prokopov <roboter@begemotik.ee>
2024-07-10 16:11:04 +0300

    Ionic capacitor barcode scanner

--- healthcheck/src/app/home/home.page.ts
+++ healthcheck/src/app/home/home.page.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-// import { AppVersion } from '@ionic-native/app-version/ngx';
-// import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
-
-// import { Plugins } from '@capacitor/core';
-// import { DatePickerPluginInterface } from '@capacitor-community/date-picker';
-// import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
-
-// const DatePicker: DatePickerPluginInterface = Plugins.DatePickerPlugin as any;
-// const selectedTheme = 'light';
+import {
+  BarcodeScanner,
+  BarcodeFormat,
+  LensFacing,
+} from '@capacitor-mlkit/barcode-scanning';
 
 @Component({
   selector: 'app-home',
@@ -30,7 +26,7 @@ export class HomePage {
     //   })
     //   .catch((error) => console.log(error));
     // Pass a custom class to each select interface for styling
-    const selects = document.querySelectorAll('.custom-options') as NodeListOf<HTMLIonSelectElement>; ;
+    const selects = document.querySelectorAll('.custom-options') as NodeListOf<HTMLIonSelectElement>;;
     console.log(selects.length);
     for (var i = 0; i < selects.length; i++) {
       selects[i].interfaceOptions = {
@@ -38,41 +34,6 @@ export class HomePage {
       };
     }
   }
-  // Pass a custom class to each select interface for styling
-
-  // scanQR() {
-  //   this.barcodeScanner
-  //     .scan()
-  //     .then((barcodeData) => {
-  //       console.log('Barcode data', barcodeData);
-  //     })
-  //     .catch((err) => {
-  //       console.log('Error', err);
-  //     });
-  //   // Optionally request the permission early
-  //   this.qrScanner
-  //     .prepare()
-  //     .then((status: QRScannerStatus) => {
-  //       if (status.authorized) {
-  //         // camera permission was granted
-
-  //         // start scanning
-  //         const scanSub = this.qrScanner.scan().subscribe((text: string) => {
-  //           console.log('Scanned something', text);
-
-  //           this.qrScanner.hide(); // hide camera preview
-  //           scanSub.unsubscribe(); // stop scanning
-  //         });
-  //       } else if (status.denied) {
-  //         // camera permission was permanently denied
-  //         // you must use QRScanner.openSettings() method to guide the user to the settings page
-  //         // then they can grant the permission from there
-  //       } else {
-  //         // permission was denied, but not permanently. You can ask for permission again at a later time.
-  //       }
-  //     })
-  //     .catch((e: any) => console.log('Error is', e));
-  // }
 
   doahealthcheck(url) {
     this.http
@@ -100,4 +61,131 @@ export class HomePage {
     //   theme: selectedTheme,
     // }).then((date) => alert(date.value));
   }
+
+  startScan = async () => {
+    // The camera is visible behind the WebView, so that you can customize the UI in the WebView.
+    // However, this means that you have to hide all elements that should not be visible.
+    // You can find an example in our demo repository.
+    // In this case we set a class `barcode-scanner-active`, which then contains certain CSS rules for our app.
+    document.querySelector('body')?.classList.add('barcode-scanner-active');
+
+    // Add the `barcodeScanned` listener
+    const listener = await BarcodeScanner.addListener(
+      'barcodeScanned',
+      async result => {
+        console.log(result.barcode);
+      },
+    );
+
+    // Start the barcode scanner
+    await BarcodeScanner.startScan();
+  };
+
+  stopScan = async () => {
+    // Make all elements in the WebView visible again
+    document.querySelector('body')?.classList.remove('barcode-scanner-active');
+
+    // Remove all listeners
+    await BarcodeScanner.removeAllListeners();
+
+    // Stop the barcode scanner
+    await BarcodeScanner.stopScan();
+  };
+
+  scanSingleBarcode = async () => {
+    return new Promise(async resolve => {
+      document.querySelector('body')?.classList.add('barcode-scanner-active');
+
+      const listener = await BarcodeScanner.addListener(
+        'barcodeScanned',
+        async result => {
+          await listener.remove();
+          document
+            .querySelector('body')
+            ?.classList.remove('barcode-scanner-active');
+          await BarcodeScanner.stopScan();
+          resolve(result.barcode);
+        },
+      );
+
+      await BarcodeScanner.startScan();
+    });
+  };
+
+  scan = async () => {
+    const { barcodes } = await BarcodeScanner.scan({
+      formats: [BarcodeFormat.QrCode],
+    });
+    return barcodes;
+  };
+
+  isSupported = async () => {
+    const { supported } = await BarcodeScanner.isSupported();
+    return supported;
+  };
+
+  enableTorch = async () => {
+    await BarcodeScanner.enableTorch();
+  };
+
+  disableTorch = async () => {
+    await BarcodeScanner.disableTorch();
+  };
+
+  toggleTorch = async () => {
+    await BarcodeScanner.toggleTorch();
+  };
+
+  isTorchEnabled = async () => {
+    const { enabled } = await BarcodeScanner.isTorchEnabled();
+    return enabled;
+  };
+
+  isTorchAvailable = async () => {
+    const { available } = await BarcodeScanner.isTorchAvailable();
+    return available;
+  };
+
+  setZoomRatio = async () => {
+    await BarcodeScanner.setZoomRatio({ zoomRatio: 0.5 });
+  };
+
+  getZoomRatio = async () => {
+    const { zoomRatio } = await BarcodeScanner.getZoomRatio();
+    return zoomRatio;
+  };
+
+  getMinZoomRatio = async () => {
+    const { zoomRatio } = await BarcodeScanner.getMinZoomRatio();
+    return zoomRatio;
+  };
+
+  getMaxZoomRatio = async () => {
+    const { zoomRatio } = await BarcodeScanner.getMaxZoomRatio();
+    return zoomRatio;
+  };
+
+  openSettings = async () => {
+    await BarcodeScanner.openSettings();
+  };
+
+  isGoogleBarcodeScannerModuleAvailable = async () => {
+    const { available } =
+      await BarcodeScanner.isGoogleBarcodeScannerModuleAvailable();
+    return available;
+  };
+
+  installGoogleBarcodeScannerModule = async () => {
+    await BarcodeScanner.installGoogleBarcodeScannerModule();
+  };
+
+  checkPermissions = async () => {
+    const { camera } = await BarcodeScanner.checkPermissions();
+    return camera;
+  };
+
+  requestPermissions = async () => {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera;
+  };
 }
